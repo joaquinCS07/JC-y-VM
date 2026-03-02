a631b2d363ad51e02eaf2fa22c0aafb538c20f02
joaquin <jcs143@alu.ua.es>
2026-03-02 14:31:54 +0100

    he añadido una clase y sus subclases

--- Clases.py
+++ Clases.py
@@ -17,6 +17,24 @@ class Evento:
     def __str__(self):
         return f"Evento: {self.nombre_evento} | Fecha: {self.fecha} | Lugar: {self.sede.nombre}"
 
+class Entrada:
+    def __init__(self, id_entrada, precio_base):
+        self.id_entrada = id_entrada
+        self.precio_base = precio_base
 
+    def __str__(self):
+        return f"Entrada #{self.id_entrada} | Precio: {self.precio_base}€"
+
+class EntradaGeneral(Entrada):
+    def __init__(self, id_entrada, precio_base):
+        # super().__init__ llama al constructor de la clase padre (Entrada)
+        super().__init__(id_entrada, precio_base)
+        self.puerta_acceso = "Puerta Principal"
+
+class EntradaVIP(Entrada):
+    def __init__(self, id_entrada, precio_base, zona_exclusiva):
+        super().__init__(id_entrada, precio_base)
+        self.puerta_acceso = "Puerta Prioritaria"
+        self.zona_exclusiva = zona_exclusiva
 
 
