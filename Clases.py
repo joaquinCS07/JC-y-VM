b9191599bad6d8857ddf5bbecc2e71e8a733494c
victor <vmal3@alu.ua.es>
2026-03-02 14:23:55 +0100

    He creado la clase Evento.

--- Clases.py
+++ Clases.py
@@ -3,3 +3,14 @@ class Sede:
         self.nombre= nombre
         self.direccion= direccion
         self.aforo_maximo= aforo_maximo
+
+
+
+class Evento:
+    def __init__(self, nombre_evento, fecha, sede):
+        self.nombre_evento = nombre_evento
+        self.fecha = fecha
+        self.sede = sede
+        self.entradas_vendidas= []
+
+
