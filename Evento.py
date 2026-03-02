dad28e0724161c2372870ff0d5804e60acc28fa6
joaquin <jcs143@alu.ua.es>
2026-03-02 14:42:18 +0100

    evento

---
+++ Evento.py
@@ -0,0 +1,6 @@
+class Evento:
+    def __init__(self, nombre_evento, fecha, sede):
+        self.nombre_evento = nombre_evento
+        self.fecha = fecha
+        self.sede = sede
+        self.entradas_vendidas= []
