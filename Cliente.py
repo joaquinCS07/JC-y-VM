74a8808d67444506b457a880e4a544c4797540f7
Victor <vmal3@alu.ua.es>
2026-03-14 19:21:30 +0100

    He creado una nueva clase llamada Cliente. También he agregado dos métodos en la clase Persona: mostrar_info y total_registrados. Este último es utilizado tambien la clase cliente.

---
+++ Cliente.py
@@ -0,0 +1,15 @@
+from Persona import Persona
+class Cliente(Persona):
+    def __init__(self, nombre, dni, email, telefono):
+
+        super().__init__(nombre, dni, email)
+        self._telefono = telefono
+        self.historial_compras = []
+
+        def nueva_compra(self, compra):
+            self.historial_compras.append(compra)
+            print(f'Compra guardada correctamente en el historial del cliente: {self._nombre}')
+
+        def mostrar_info(self):
+            info_persona = super().mostrar_info()
+            return f'{info_persona}, Teléfono: {self._telefono}, Número de compras realizadas: {len(self._historial_compras)} '
--- Persona.py
+++ Persona.py
@@ -1,7 +1,17 @@
 class Persona:
 
-    def __init__(self, nombre, dni):
+    total_personas = 0
+
+    def __init__(self, nombre, dni, correo):
         self._nombre = nombre
         self._dni = dni
-        self._email = email
+        self._correo = correo
+        Persona.total_personas += 1
+
+    def mostrar_info(self):
+        return f'Nombre: {self._nombre}, dni: {self._dni}, correo electrónico: {self._correo}'
+
+    @classmethod
+    def total_registrados(cls):
+        return cls.total_personas
 
