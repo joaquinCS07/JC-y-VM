27bcf8a9cd30d653c1ddffbe43359abfc2e18f24
joaquincs <jcs143@alu.ua.es>
2026-03-14 19:30:50 +0100

    He creado las 2 subclases EntradaGeneral y EntradaVip, vienen de Entrada

--- Entrada.py
+++ Entrada.py
@@ -6,14 +6,5 @@ class Entrada:
     def __str__(self):
         return f"Entrada #{self.id_entrada} | Precio: {self.precio_base}€"
 
-class EntradaGeneral(Entrada):
-    def __init__(self, id_entrada, precio_base):
-        # super().__init__ llama al constructor de la clase padre (Entrada)
-        super().__init__(id_entrada, precio_base)
-        self.puerta_acceso = "Puerta Principal"
 
-class EntradaVIP(Entrada):
-    def __init__(self, id_entrada, precio_base, zona_exclusiva):
-        super().__init__(id_entrada, precio_base)
-        self.puerta_acceso = "Puerta Prioritaria"
-        self.zona_exclusiva = zona_exclusiva
+
