22b323a9a5d4ffd5bc42a34272ce0abe656a6219
joaquincs <jcs143@alu.ua.es>
2026-03-14 19:31:03 +0100

    He creado las 2 subclases EntradaGeneral y EntradaVip, vienen de Entrada

---
+++ EntradaVip.py
@@ -0,0 +1,9 @@
+from Entrada import Entrada
+class EntradaVIP(Entrada):
+    def __init__(self, id_entrada, precio_base, zona_exclusiva):
+        super().__init__(id_entrada, precio_base)
+        self.puerta_acceso = "Puerta Prioritaria"
+        self.zona_exclusiva = zona_exclusiva
+
+    def calcular_precio_final(self):
+        return self.precio_base + 50
