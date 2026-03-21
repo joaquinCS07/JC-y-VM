66514a3fcdefe273a5f93a21a269bcb86c3cd5ec
Victor <vmal3@alu.ua.es>
2026-03-21 18:07:27 +0100

    He modificado las clases: Cliente y Sede. En la clase cliente, primeramente he agregado un nuevo método (mostrar_info), y he realizado cambios en los atributos para cumplir con el encapsulamiento. En la clase sede, también he realizado cambios para el encapsulamiento, he agregado el guión bajo a los atributos y he establecido las propiedades abajo para el uso adecuado de los atributos privados.

--- cliente.py
+++ cliente.py
@@ -1,15 +1,24 @@
 from persona import Persona
+
 class Cliente(Persona):
     def __init__(self, nombre, dni, email, telefono):
-
         super().__init__(nombre, dni, email)
         self._telefono = telefono
-        self.historial_compras = []
+        self._historial_compras = []
 
-        def nueva_compra(self, compra):
-            self.historial_compras.append(compra)
-            print(f'Compra guardada correctamente en el historial del cliente: {self._nombre}')
+    def nueva_compra(self, compra):
+        self._historial_compras.append(compra)
+        print(f'Compra guardada para: {self._nombre}')
 
-        def mostrar_info(self):
-            info_persona = super().mostrar_info()
-            return f'{info_persona}, Teléfono: {self._telefono}, Número de compras realizadas: {len(self._historial_compras)} '
+    def mostrar_info(self):
+        info_persona = super().mostrar_info()
+        return f'{info_persona}, Teléfono: {self._telefono}, Compras: {len(self._historial_compras)}'
+
+
+@property
+def telefono(self):
+    return self._telefono
+
+@property
+def historial_compras(self):
+    return self._historial_compras
--- sede.py
+++ sede.py
@@ -1,8 +1,21 @@
 class Sede:
     def __init__(self, nombre, direccion, aforo_maximo):
-        self.nombre= nombre
-        self.direccion= direccion
-        self.aforo_maximo= aforo_maximo
+        self._nombre= nombre
+        self._direccion= direccion
+        self._aforo_maximo= aforo_maximo
 
     def __str__(self):
         return f"Sede: {self.nombre} | Aforo: {self.aforo_maximo} personas"
+
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @property
+    def aforo_maximo(self):
+        return self._aforo_maximo
+
+    @property
+    def direccion(self):
+        return self._direccion
+
