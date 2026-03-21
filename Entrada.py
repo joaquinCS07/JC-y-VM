2cb7f2300786d7bdd796f4b8058d0263ee70a9ef
joaquincs <jcs143@alu.ua.es>
2026-03-21 18:02:08 +0100

    He modificado la clase Entrada, ahora ha pasado a ser una clase abstracta. Además, he añadido un metodo abstracto: calcular_precio_final. Por último, he añadido un método para marcar una entrada como vendida

--- Entrada.py
+++ Entrada.py
@@ -1,10 +1,21 @@
-class Entrada:
+from abc import ABC, abstractmethod
+
+class Entrada(ABC):
     def __init__(self, id_entrada, precio_base):
-        self.id_entrada = id_entrada
-        self.precio_base = precio_base
+        self._id_entrada = id_entrada
+        self._precio_base = precio_base
+        self._vendida = False
+
+    @abstractmethod
+    def calcular_precio_final(self):
+        pass
+
+    def marcar_como_vendida(self):
+        self._vendida = True
 
     def __str__(self):
-        return f"Entrada #{self.id_entrada} | Precio: {self.precio_base}€"
+        estado = 'Vendida' if self._vendida else 'Disponible'
+        return f"Entrada #{self.id_entrada} | Precio: {self.precio_base}€ | {estado}"
 
 
 
