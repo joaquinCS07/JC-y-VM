eeee6309bb41e6c9572fc917a3cbb419a9da5657
victormalopez <vmal3@alu.ua.es>
2026-05-11 09:06:15 +0200

    He arreglado errores en el archivo evento.py

--- evento.py
+++ evento.py
@@ -7,8 +7,19 @@ class Evento:
 
     def calcular_ingresos_totales(self):
         total = 0
-        for entrada in self.entradas_vendidas:
+        for entrada in self._entradas_vendidas:
             total += entrada.calcular_precio()
         return total
+
+    def agregar_venta(self, entrada): 
+        self._entradas_vendidas.append(entrada)
+        entrada.marcar_como_vendida() 
+
+
+    def __str__(self):
+        return f"{self._nombre_evento} - {self._fecha} - {self._sede.nombre}"
+     
+    def __add__(self, otro_evento):
+        return self.calcular_ingresos_totales() + otro_evento.calcular_ingresos_totales()
 
 
